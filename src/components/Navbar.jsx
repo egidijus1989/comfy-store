074e0db8691ad37e6d3bdf4723901d9c1947ff77
egidijus <145454567+egidijus1989@users.noreply.github.com>
2025-02-08 21:40:01 +0200

    about page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 import { FaBarsStaggered } from "react-icons/fa6";
+import { NavLinks } from ".";
+import { useEffect, useState } from "react";
+
+const themes = {
+  retro: "retro",
+  luxury: "luxury",
+};
+
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem("theme") || themes.retro;
+};
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+  const handleTheme = () => {
+    const { retro, luxury } = themes;
+    const newTheme = theme === retro ? luxury : retro;
+
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }),
+    [theme];
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
@@ -21,17 +45,24 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              nav links
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal">nav links</ul>
+          <ul className="menu menu-horizontal">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/*THEME SETUP*/}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            <BsSunFill className="swap-on h-4 w-4" />
+            <BsMoonFill className="swap-off h-4 w-4" />
+          </label>
           {/*CART LINK*/}
           <NavLink to="/cart" className="btn btn-ghost btn-circle mtn-md ml-4">
             <div className="indicator">
